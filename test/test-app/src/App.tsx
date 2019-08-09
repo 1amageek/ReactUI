3ad1fa36a4f8638921b0b0d1b15851a1e61197a1
1amageek <tmy0x3@icloud.com>
2019-08-09 16:50:40 +0900

    Added ScrollView

--- test/test-app/src/App.tsx
+++ test/test-app/src/App.tsx
@@ -1,61 +1,100 @@
 import React from 'react';
 import './App.css';
 
-import { HostView, Spacer, Text, HStack, VStack, NavigationView, List } from 'web-ui'
+import { HostView, Spacer, Text, HStack, VStack, NavigationView, ScrollView, List } from 'web-ui'
 
 const App: React.FC = () => {
   return (
     <HostView style={{ margin: 0, padding: 0, height: "100%", width: "100%", position: "absolute" }}>
 
-      <VStack>
-        <List>
-          <Text>aaaa</Text>
-          <Text>aaaa</Text>
-          <Text>aaaa</Text>
-        </List>
-      </VStack>
-
-
-
-      {/* <VStack style={{ background: "green" }}>
-        <Spacer />
-        <HStack style={{ background: "red" }}>
-          <p>Hello World</p>
-          <Spacer />
-          <p>Hello World</p>
-        </HStack>
+      {/* <VStack>
+        <ScrollView style={{background: "red"}}>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+          <Text>hoge</Text>
+        </ScrollView>
         <Spacer />
       </VStack> */}
 
-      {/* <VStack style={{ background: "green" }}>
-        <HStack style={{ background: "blue" }}>
-          <Text>Hello World</Text>
+
+      {/* <List>
+        <HStack>
+          <Text style={{paddingLeft: "100px"}}>hoge</Text>
           <Spacer />
-          <Text>Hello World</Text>
+          <Text>hoge</Text>
+        </HStack>
+        <HStack>
+          <Text style={{paddingLeft: "100px"}}>hoge</Text>
+          <Spacer />
+          <Text>hoge</Text>
+        </HStack>
+        <HStack>
+          <Text style={{paddingLeft: "100px"}}>hoge</Text>
+          <Spacer />
+          <Text>hoge</Text>
+        </HStack>
+      </List> */}
+
+      {/* <VStack>
+        <HStack style={{ background: "green" }}>
+          <Text>Hoge</Text>
+          <Spacer />
+          <Text>Hoge</Text>
         </HStack>
         <VStack style={{ background: "red" }}>
-          <Text>Hello World</Text>
+          <Text>Hoge</Text>
           <Spacer />
-          <Text>Hello World</Text>
+          <Text>Hoge</Text>
         </VStack>
       </VStack> */}
 
-      {/* <VStack style={{ background: "red" }}>
-        <Text>Hello World</Text>
-        <Spacer />
-        <Text>Hello World</Text>
-        <VStack style={{ background: "green" }}>
-          <Text>Hello World</Text>
-          <Spacer />
-          <Text>Hello World</Text>
-        </VStack>
-      </VStack> */}
-
-      {/* <HStack>
-        <Text>Hello World</Text>
-        <Spacer />
-        <Text>Hello World</Text>
-      </HStack> */}
 
     </HostView>
   );
