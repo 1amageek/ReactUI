321065bc75324d6b41175b7b2feca8136375e776
1amageek <tmy0x3@icloud.com>
2019-08-11 13:16:31 +0900

    Fix

--- test/test-app/src/App.tsx
+++ test/test-app/src/App.tsx
@@ -7,77 +7,13 @@ const App: React.FC = () => {
   return (
     <HostView style={{ margin: 0, padding: 0, height: "100%", width: "100%", position: "absolute" }}>
 
-      {/* <HStack>
-        <List>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-        </List>
-        <Text>hoge</Text>
-      </HStack> */}
-      <VStack>
-        <List>
-          <Text>hoge</Text>
-        </List>
-        <ScrollView style={{ background: "red" }}>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-          <Text>hoge</Text>
-        </ScrollView>
-
-      </VStack>
-
-
-      {/* <List>
+      <List>
         <HStack>
-          <Text style={{paddingLeft: "100px"}}>hoge</Text>
-          <Spacer />
+          <Text>hoge</Text>
+          <Spacer/>
           <Text>hoge</Text>
         </HStack>
-        <HStack>
-          <Text style={{paddingLeft: "100px"}}>hoge</Text>
-          <Spacer />
-          <Text>hoge</Text>
-        </HStack>
-        <HStack>
-          <Text style={{paddingLeft: "100px"}}>hoge</Text>
-          <Spacer />
-          <Text>hoge</Text>
-        </HStack>
-      </List> */}
-
-      {/* <VStack>
-        <HStack style={{ background: "green" }}>
-          <Text>Hoge</Text>
-          <Spacer />
-          <Text>Hoge</Text>
-        </HStack>
-        <VStack style={{ background: "red" }}>
-          <Text>Hoge</Text>
-          <Spacer />
-          <Text>Hoge</Text>
-        </VStack>
-      </VStack> */}
-
+      </List>
 
     </HostView>
   );
