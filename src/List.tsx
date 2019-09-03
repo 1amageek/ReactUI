501f873bddc528f567ee95cf7f63fc4ae8e288cc
1amageek <tmy0x3@icloud.com>
2019-09-03 14:27:17 +0900

    Fix

--- src/List.tsx
+++ src/List.tsx
@@ -27,16 +27,29 @@ export const List = ({ children, style }: { children: any, style?: CSSProperties
 			})
 		}
 	}
+	
+	const cleanChild = (element: any): any => {
+		if (element.type === "a") {
+			return element.props.children
+		} else if (element.props.href) {
+			return cleanChild(element.props.children)
+		}
+		return element
+	}
 
 	return (
 		<ScrollView axes={[Axis.horizontal, Axis.vertical]} style={style}>
 			<ul className="list column expandable horizontal vertical" ref={ref}>
 				{React.Children.map(children, (child, index) => {
+
+					let children = child
+					
+
 					return (
 						<li className="list-cell row expandable horizontal" key={`${index}`} style={style}>
 							<a className="row expandable horizontal" onClick={child.props.onClick} href={child.props.href}>
 								<div className="list-cell-content-view row expandable horizontal" style={{ paddingLeft: "16px", paddingRight: "16px", paddingTop: "4px", paddingBottom: "4px" }} ref={contetViewRef}>
-									{child.type === "a" ? child.props.children : child}
+									{cleanChild(child)}
 								</div>
 							</a>
 						</li>
