797b6c12e0bf018a496a6af98c41b9fb0362f46b
1amageek <tmy0x3@icloud.com>
2019-09-06 15:53:44 +0900

    Fix

--- src/NavigationView.tsx
+++ src/NavigationView.tsx
@@ -7,13 +7,9 @@ export const NavigationView = ({ children, style }: { children: any, style?: CSS
 	_style.paddingTop = "96px"
 
 	const ref = (element: HTMLDivElement | null) => {
-		if (element) {
-			const elements = Array.from(element.children).filter(item => !item.className.includes("applicatal") &&
-				!item.className.includes("navigation-bar") &&
-				(item.className.includes("expandable") && item.className.includes("vertical")))
-			// elements.forEach(item => (item as HTMLElement).style.paddingTop = "96px")
-			prepareLayout(element)
-			layout(element)
+		if (element && element.parentElement) {
+			prepareLayout(element.parentElement)
+			layout(element.parentElement)
 		}
 	}
 
