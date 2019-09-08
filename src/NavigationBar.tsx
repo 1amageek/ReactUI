1d2214b12d777a82db304eb40f7ebed149d39089
1amageek <tmy0x3@icloud.com>
2019-09-08 17:52:17 +0900

    Fix

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,12 +1,20 @@
 import React, { CSSProperties } from "react"
+import { prepareLayout, layout } from "./LayoutEngine"
 
 export const NavigationBar = ({ children, style }: { children: any, style?: CSSProperties }) => {
 	const _style: CSSProperties = {...style}
 	_style.height = "96px"
 	_style.top = 0
 
+	const ref = (element: HTMLDivElement | null) => {
+		if (element) {
+			prepareLayout(element)
+			layout(element)
+		}
+	}
+
 	return (
-		<div className="navigation-bar expandable horizontal applicatal" style={_style}>
+		<div className="navigation-bar expandable horizontal applicatal" style={_style} ref={ref}>
 			{children}
 		</div>
 	)
